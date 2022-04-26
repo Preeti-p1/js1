2184e15136a96bfaae1faf0cec8a8476f197a0bb
Preeti-p1 <102037183+Preeti-p1@users.noreply.github.com>
2022-04-26 20:53:31 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,51 @@
+console.log("working")
+//bitwise operator
+console.log(2 | 3);
+console.log(2 ^ 3);
+console.log(2 & 3);
+console.log(2 << 3);
+console.log(2 >> 3);
+
+
+
+let g = 3;
+
+//if(g>4)
+//{
+//console.log(g);
+//}
+//else{
+//console.log("enter the else ");
+//}
+
+
+if (g > 4) {
+    console.log(g);
+}
+else if (g == 3) {
+    console.log("value of g");
+}
+else {
+    console.log("enter the else ");
+}
+
+
+let number = 3;
+switch (number)
+ {
+    case 1:
+        console.log("i am one");
+        break;
+    case 2:
+        console.log("i am two");
+        break;
+    case 3:
+        console.log("i am three");
+        break;
+    case 4:
+        console.log("i am four");
+        break;
+    default:
+        console.log("default value");
+        break;
+}
